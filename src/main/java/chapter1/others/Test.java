a0f8f3fadca43d5bf9e0d73d712efb3ce9a2e5c9
liuduo <253444990@qq.com>
2014-06-04 22:23:40 +0800

    Test类更改

--- src/main/java/chapter1/others/Test.java
+++ src/main/java/chapter1/others/Test.java
@@ -1,6 +1,5 @@
 package chapter1.others;
 
-import edu.princeton.cs.introcs.StdOut;
 
 public class Test {
 	public static int mystery(int a, int b) {
@@ -40,6 +39,35 @@ public class Test {
 	}
 	
 	public static void main(String[] args) {
-		StdOut.println(1+2+"3");
+//		double t = 9.0;
+//		while (Math.abs(t-9.0/t)>.001)
+//			t=(9.0/t+t)/2.0;
+//		StdOut.printf("%.5f\n", t);
+		
+//		int sum = 0;
+//		for (int i=1;i<1000;i++)
+//			for (int j=0;j<i;j++)
+//				sum++;
+//		StdOut.println(sum);
+		
+//		int sum=0;
+//		for (int i=1;i<1000;i*=2)
+//			for (int j=0;j<1000;j++)
+//				sum++;
+//		StdOut.println(sum);
+		
+//		System.out.println((char)('a'+4));
+		
+//		int[] a = new int[10];
+//		for (int i=0;i<10;i++)
+//			a[i]=9-i;
+//		for (int i=0;i<10;i++)
+//			a[i]=a[a[i]];
+//		for (int i=0;i<10;i++)
+//			System.out.println(i);
+		
+//		System.out.println(exR1(6));
+		
+		System.out.println(mystery(3, 11));
 	}
 }
