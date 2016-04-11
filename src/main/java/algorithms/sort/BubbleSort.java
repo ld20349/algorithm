e69209c925eb851f29f186d54c7ddd43eeda4d0c
liuduo-jpdr <liuduo@jupaidaren.com>
2016-04-11 17:51:38 +0800

    冒泡排序算法更简练

--- src/main/java/algorithms/sort/BubbleSort.java
+++ src/main/java/algorithms/sort/BubbleSort.java
@@ -10,8 +10,8 @@ public class BubbleSort extends Sort {
 
 	@Override
 	public void sort(int[] a) {
-		for (int i = a.length - 2; i >= 1; i--) {
-			for (int j = 0; j <= i; j++) {
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = 0; j < a.length - 1 - i; j++) {
 				if (a[j] > a[j + 1]) {
 					swap(a, j, j + 1);
 				}
