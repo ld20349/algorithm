cb4b5fdd5b9a74dca6ed688761c402be3c6038d6
liuduo-jpdr <liuduo@jupaidaren.com>
2016-04-14 20:15:46 +0800

    精简了计数排序的代码。

--- src/main/java/algorithms/sort/CountingSort.java
+++ src/main/java/algorithms/sort/CountingSort.java
@@ -13,26 +13,20 @@ public class CountingSort extends Sort {
 		int min = a[0];
 		int max = a[0];
 		for (int i = 1; i < a.length; i++) {
-			if (a[i] < min) {
+			if (a[i] < min)
 				min = a[i];
-			}
-			if (a[i] > max) {
+			if (a[i] > max)
 				max = a[i];
-			}
 		}
 		int[] c = new int[max - min + 1];
-		for (int e : a) {
+		for (int e : a)
 			c[e - min]++;
-		}
-		for (int i = 1; i < c.length; i++) {
-			c[i] = c[i] + c[i - 1];
-		}
+		for (int i = 1; i < c.length; i++)
+			c[i] += c[i - 1];
 		int[] r = new int[a.length];
-		for (int e : a) {
-			r[c[e - min] - 1] = e;
-			c[e - min]--;
-		}
-		System.arraycopy(r, 0, a, 0, r.length);
+		for (int e : a)
+			r[--c[e - min]] = e;
+		System.arraycopy(r, 0, a, 0, a.length);
 	}
 
 	public static void main(String[] args) {
