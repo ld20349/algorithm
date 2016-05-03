1fbef9f3c7f2d2cad8104b2491469b685511fd1e
liuduo-jpdr <liuduo@jupaidaren.com>
2016-05-03 22:00:20 +0800

    链表的实现做了微调.

--- src/main/java/datastructures/list/doubly/Node.java
+++ src/main/java/datastructures/list/doubly/Node.java
@@ -10,33 +10,4 @@ class Node {
         this.prev = prev;
         this.next = next;
     }
-
-    public Object getElement() {
-        return element;
-    }
-
-    public void setElement(Object element) {
-        this.element = element;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    public Node getPrev() {
-        return prev;
-    }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
-    @Override
-    public String toString() {
-        return element.toString();
-    }
 }
--- src/main/java/datastructures/list/single/Node.java
+++ src/main/java/datastructures/list/single/Node.java
@@ -8,25 +8,4 @@ class Node {
         this.element = element;
         this.next = next;
     }
-
-    public Object getElement() {
-        return element;
-    }
-
-    public void setElement(Object element) {
-        this.element = element;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    @Override
-    public String toString() {
-        return element.toString();
-    }
 }
